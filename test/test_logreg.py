4f61073980dd20d8431ced35d6256a986681a412
H Miller <haleighmiller366@gmail.com>
2022-02-23 23:53:40 -0800

    new tests

--- test/test_logreg.py
+++ test/test_logreg.py
@@ -1,24 +1,19 @@
 import regression
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 def model_for_testing():
         X_train, X_test, y_train, y_test = regression.loadDataset(features=['Penicillin V Potassium 500 MG',
                                                                                   'Computed tomography of chest and abdomen',
                                                                                   'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
                                                                                   'Creatinine'], split_percent=0.8)
-        lr = regression.LogisticRegression(X_train.shape[1])
+        lr = regression.LogisticRegression(X_train.shape[1]) #default parameters
         lr.train_model(X_train, y_train, X_test, y_test)
         return lr
 
 def test_updates():
         ### load data for testing ###
         lr=model_for_testing()
-        
-        # Check that your gradient is being calculated correctly
-        
-
-	# Check that your loss function is correct and that 
-	# you have reasonable losses at the end of training
         
 	### check that loss decreases ###
         loss_hist=lr.loss_history_train
@@ -31,12 +26,24 @@ def test_updates():
 
         ### check loss decreasing another way ###
         deltas=[loss_hist[i+1]-loss_hist[i] for i in range(len(loss_hist)-1)]
-        assert np.mean(deltas)<0, "Loss sequence not decreaseing"
+        assert np.mean(deltas)<0, "Loss sequence not decreasing"
 
 def test_predict():
-	# Check that self.W is being updated as expected
-	# and produces reasonable estimates for NSCLC classification
+        X_train, X_test, y_train, y_test = regression.loadDataset(features=['Penicillin V Potassium 500 MG',
+                                                                                  'Computed tomography of chest and abdomen',
+                                                                                  'Plain chest X-ray (procedure)', 'Low Density Lipoprotein Cholesterol',
+                                                                                  'Creatinine'], split_percent=0.8)
+        
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform (X_test)
+        
 
-	# Check accuracy of model after training
+        lr = regression.LogisticRegression(X_train.shape[1]) #default parameters
+        lr.train_model(X_train, y_train, X_test, y_test)
 
-	pass
+        ### test that predictions are somewhat accurate ###
+        X_test=np.column_stack((X_test, np.ones(X_test.shape[0]))) ## add in bias
+        y_pred=lr.make_prediction(X_test) ## predict the values of the validation set
+        MSE=np.mean((y_pred-y_test)**2)
+        assert MSE < 0.25 
